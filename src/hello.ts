b1bd055fe6ffcb07e7831e088830bb8decce0fb7
Reinaldo <101670067+cortezcodar@users.noreply.github.com>
2022-07-27 20:48:56 -0300

    feat/estrutura-inicial
    hello-word kevin

---
+++ src/hello.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+
+app.use(cors({
+  origin: ['*'],
+}));
+
+
+app.get('/', (req, res) => {
+  res.send('<h2> Hello word, kevin</h2>');
+});
+app.listen(3000, () =>{
+  console.log('server listening at port 3000 at localhost');
+});
