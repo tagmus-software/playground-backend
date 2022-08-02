356f715020bb89df40b2363c88de385f448a88b8
Reinaldo <101670067+cortezcodar@users.noreply.github.com>
2022-08-02 16:24:08 -0300

    feat: [PB-3] Implementar o typeORM (#3)
    * feat: [PB-3] Implementar o typeORM
    
    * feat:[PB-3] Reorganizando os codigos
    
    * feat:[PB-3] -import-reflect-metadata
    
    * feat:[PB-3] -import-reflect-metadata
    
    * feat:[PB-3]-atualização-dos-arquivos- env-services

---
+++ src/data-soucer.ts
@@ -0,0 +1,21 @@
+import { DataSource } from "typeorm";
+import 'reflect-metadata'
+import 'dotenv'
+
+const port = process.env.DB_PORT as number | undefined
+
+export const appDataSource = new DataSource ({
+ type: "mysql",
+ host:process.env.DB_HOST,
+ port:3306,
+ username:process.env.DB_USER,
+ password:process.env.DB_PASS,
+ database:process.env.DB_NAME,
+
+ 
+ entities: [`${__dirname}/**/entities/*.{ts,js}`],
+ synchronize: true,
+ logging: false,
+ 
+
+});
---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import 'reflect-metadata'
+import express from 'express';
+import cors from 'cors';
+import { appDataSource } from './data-soucer';
+
+appDataSource.initialize().then(() => {
+
+const app = express();
+ 
+app.use(express.json())
+
+app.use(cors({
+  origin: ['*']
+}));
+
+ //hello
+app.get('/', (req, res) => {
+ return res.json('Hello kevin');
+});
+ return app.listen(process.env.PORT)
+
+  });
+ 
+
