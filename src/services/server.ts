77f6f1c95477d67601ed331ae25b46715ad40b4d
Reinaldo <101670067+cortezcodar@users.noreply.github.com>
2022-08-01 23:33:43 -0300

    feat:[PB-3] Reorganizando os codigos

---
+++ src/services/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import cors from 'cors';
+import { appDataSource } from './data-soucer';
+import 'reflect-metadata'
+
+
+
+
+appDataSource.initialize().then(() => {
+
+const app = express();
+ 
+app.use(express.json())
+
+app.use(cors({
+  origin: ['*']
+}));
+
+ //hello
+app.get('/', (req, res) => {
+ return res.json('Hello kevin');
+});
+ return app.listen(process.env.PORT)
+
+  });
+ 
+
