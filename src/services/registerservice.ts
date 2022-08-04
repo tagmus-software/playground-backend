800b449e552d7d57d15f8c1d670a6900ab5ba462
Reinaldo <101670067+cortezcodar@users.noreply.github.com>
2022-08-03 22:23:18 -0300

    feat:[PB-4]-Criar-endpoint-para-registrar-usuarios

--- src/services/registerservice.ts
+++ src/services/registerservice.ts
@@ -1,3 +1,10 @@
-import { Seeder } from "typeorm-extension";
+import { DataSource } from "typeorm";
+import { Seeder, SeederFactoryManager } from "typeorm-extension";
 
-export class registerservice
+export class registerservice implements Seeder{
+  async(dataSource: DataSource, factoryManager: SeederFactoryManager): Promise<void> {
+    throw new Error("Method not implemented.");
+    const user
+  }
+
+}
