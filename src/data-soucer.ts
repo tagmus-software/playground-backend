7186662289d222c3b50a749b0eea93e75cbff256
Reinaldo <101670067+cortezcodar@users.noreply.github.com>
2022-08-05 15:29:36 -0300

    chore:[PB-8] tabela unica de usuario

--- src/data-soucer.ts
+++ src/data-soucer.ts
@@ -1,19 +1,16 @@
 import { DataSource } from "typeorm";
-import 'reflect-metadata'
-import 'dotenv'
 
-const port = process.env.DB_PORT as number | undefined
-
+const port = +process.env.DB_PORT 
 export const appDataSource = new DataSource ({
  type: "mysql",
  host:process.env.DB_HOST,
- port:3306,
+ port:port,
  username:process.env.DB_USER,
  password:process.env.DB_PASS,
  database:process.env.DB_NAME,
 
  
- entities: [`${__dirname}/**/entities/*.{ts,js}`],
+ entities: [`${__dirname}/models/**/*.{ts,js}`],
  synchronize: true,
  logging: false,
  
---
+++ src/models/user.model.ts
@@ -0,0 +1,20 @@
+import { Column, Entity,  PrimaryGeneratedColumn  } from "typeorm";
+
+@Entity("users")
+export class User{
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({type:'varchar'})
+  name:string
+
+  @Column({type: 'varchar', unique:true})
+  email:string
+
+  @Column({type:'varchar'})
+  password:string
+
+
+
+}
+
