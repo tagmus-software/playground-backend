5ef2ceb46166c7b4ea8a396bd0b3c14ee05f7ef8
Reinaldo <101670067+cortezcodar@users.noreply.github.com>
2022-08-19 14:51:31 -0300

    feat: [PB-7] url deve ser localhost:3000/login

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,8 @@
 import { request, Request, response, Response, Router } from "express";
 import { RegisterController, } from "./controllers/register.controllers";
+const jwt = require('jsonwebtoken')
 
 
-[]
 const router = Router()
 
 const registerController = new RegisterController()
@@ -11,6 +11,21 @@ router.post('/register', async (req: Request, res: Response) => {
 
     await registerController.registra(req, res)
 })
+
+
+router.get('/login', async (req: Request, res: Response) => {
+
+    res.json([{ id: 1, email: 'covalick113@gmail.com', password: 'covalick123' }]);
+    if (req.body.email === 'Covalick113@gmail.com' && req.body.password === 'covalick123') {
+
+        await registerController.registra(req, res)
+    }
+
+
+})
+
+
+
 
 
 export default router
