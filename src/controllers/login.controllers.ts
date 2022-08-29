334792b260f6a2f6e3a93651bc03f57d31ed5596
Reinaldo <101670067+cortezcodar@users.noreply.github.com>
2022-08-29 17:22:37 -0300

    feat: [PB-7] url deve ser localhost:3000/login (#8)
    * feat: [PB-7] url deve ser localhost:3000/login
    
    * feat: [PB-7] login do Usuário
    
    * feat: [PB-7] Ajustando Codigo comentado
    
    * Update login.controllers.ts

---
+++ src/controllers/login.controllers.ts
@@ -0,0 +1,43 @@
+
+import { Request, Response } from "express";
+import { sign } from "jsonwebtoken";
+import { LoginService } from "../service/login.service";
+
+export class LoginController {
+    private service: LoginService
+
+
+    constructor() {
+        this.service = new LoginService();
+    }
+
+    public async authenticate(req: Request, res: Response) {
+
+
+        const { email, password, } = req.body;
+
+
+
+        if (!email) {
+            return res.status(400).json({ msg: 'E-email Obrigatorio!' })
+
+        }
+
+        if (!password) {
+            return res.status(400).json({ msg: 'senha obrigatória!' })
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,9 @@
 import { request, Request, response, Response, Router } from "express";
 import { RegisterController, } from "./controllers/register.controllers";
+import { LoginController } from "./controllers/login.controllers";
 
 
-[]
+
 const router = Router()
 
 const registerController = new RegisterController()
@@ -11,6 +12,19 @@ router.post('/register', async (req: Request, res: Response) => {
 
     await registerController.registra(req, res)
 })
+
+
+
+const loginController = new LoginController()
+
+router.post('/login', async (req: Request, res: Response) => {
+
+    await loginController.authenticate(req, res)
+})
+
+
+
+
 
 
 export default router
